0587f0725eb3e78972a4865f7525e85c7a5e00a3
Matt Velic <mattvelic@gmail.com>
2025-05-03 12:28:49 -0400

    dbt setup pre-class

---
+++ README.md
@@ -0,0 +1,19 @@
+# zero-to-dbt
+
+Walkthru of exercises for the [zero-to-dbt online course](https://docs.getdbt.com/guides/snowflake?_hsmi=355103390&step=1) offered by dbt.
+
+## Welcome to your new dbt project!
+
+### Using the starter project
+
+Try running the following commands:
+- dbt run
+- dbt test
+
+
+### Resources:
+- Learn more about dbt [in the docs](https://docs.getdbt.com/docs/introduction)
+- Check out [Discourse](https://discourse.getdbt.com/) for commonly asked questions and answers
+- Join the [dbt community](https://getdbt.com/community) to learn from other analytics engineers
+- Find [dbt events](https://events.getdbt.com) near you
+- Check out [the blog](https://blog.getdbt.com/) for the latest news on dbt's development and best practices
---
+++ snowflake-setup/step3.sql
@@ -0,0 +1,60 @@
+-- Create a new warehouse, two databases, and two schemas
+create warehouse transforming;
+create database raw;
+create database analytics;
+create schema raw.jaffle_shop;
+create schema raw.stripe;
+
+-- Create and load data into 3 tables in the raw schema
+create table raw.jaffle_shop.customers 
+( id integer,
+  first_name varchar,
+  last_name varchar
+);
+
+copy into raw.jaffle_shop.customers (id, first_name, last_name)
+from 's3://dbt-tutorial-public/jaffle_shop_customers.csv'
+file_format = (
+    type = 'CSV'
+    field_delimiter = ','
+    skip_header = 1
+);
+
+create table raw.jaffle_shop.orders
+( id integer,
+  user_id integer,
+  order_date date,
+  status varchar,
+  _etl_loaded_at timestamp default current_timestamp
+);
+
+copy into raw.jaffle_shop.orders (id, user_id, order_date, status)
+from 's3://dbt-tutorial-public/jaffle_shop_orders.csv'
+file_format = (
+    type = 'CSV'
+    field_delimiter = ','
+    skip_header = 1
+);
+
+create table raw.stripe.payment 
+( id integer,
+  orderid integer,
+  paymentmethod varchar,
+  status varchar,
+  amount integer,
+  created date,
+  _batched_at timestamp default current_timestamp
+);
+
+copy into raw.stripe.payment (id, orderid, paymentmethod, status, amount, created)
+from 's3://dbt-tutorial-public/stripe_payments.csv'
+file_format = (
+    type = 'CSV'
+    field_delimiter = ','
+    skip_header = 1
+);
+
+-- verify data has been loaded
+select * from raw.jaffle_shop.customers;
+select * from raw.jaffle_shop.orders;
+select * from raw.stripe.payment;
